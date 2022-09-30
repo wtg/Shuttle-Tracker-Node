1c9921cf51906c533370acfac7df556daf1035e8
edited <1084929656lkh@gmail.com>
2022-09-29 21:06:06 -0400

    Update BLE_iBeacon.ino

--- BLE_iBeacon/BLE_iBeacon.ino
+++ BLE_iBeacon/BLE_iBeacon.ino
@@ -11,54 +11,53 @@
 //RTC SLOW MEME SECTION
 RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
 RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
+RTC_DATA_ATTR static uint32_t busID =0x0010;     // 32 byte major id
 
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
 BLEAdvertising *pAdvertising;   // BLE Advertisement type
 
 struct timeval now;
 
-
-#define BEACON_UUID "87b99b2c-90fd-11e9-bc42-526af7764f64" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
+// https://www.uuidgenerator.net/
+#define BEACON_UUID "278a19f0-4056-11ed-b878-0242ac120002" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
 
 
 void setBeacon() {
-
-
   BLEBeacon oBeacon = BLEBeacon();
   oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
   oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-  oBeacon.setMajor(0x1234);//0x0102
-  oBeacon.setMinor(0x1234);
+  oBeacon.setMajor(busID);//0x0102
+  oBeacon.setMinor(0x0001);
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
   BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
-  oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04, not classic bluetooth
-  std::string strServiceData = "";
-  strServiceData += (char)26;     // Len
-  strServiceData += (char)0xFF;   // Type
-  //Customize message here
-  strServiceData += oBeacon.getData(); 
-  //strServiceData +=(char)0x1234567890ABCDEF12345678;
-  //Customize message end
-  oAdvertisementData.addData(strServiceData);
+  oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04, meaning not classic bluetooth
+  //std::string strServiceData = "";
+  //strServiceData += (char)26;     // Len
+  //strServiceData += (char)0xFF;   // Type
+  ////Customize message here
+  //strServiceData += oBeacon.getData(); 
+  ////Customize message end
+  ////concatinate customized message into servicedata
+  //oAdvertisementData.addData(strServiceData);
   pAdvertising->setAdvertisementData(oAdvertisementData);
   pAdvertising->setScanResponseData(oScanResponseData);
 }
 
-
-void setup() {
-
-
-  pinMode (2, OUTPUT);
-  digitalWrite(2, HIGH);
+void init(){
+  //init things here, including GPIO
   Serial.begin(115200);
-  gettimeofday(&now, NULL);
+  pinMode (2, OUTPUT);//LED
+}
+
+
+void bluetoothboardcasting(){
+  digitalWrite(2, HIGH);
   Serial.printf("start ESP32 %d\n", bootcount++);
   Serial.printf("deep sleep (%lds since last reset, %lds since last boot)\n", now.tv_sec, now.tv_sec - last);
   last = now.tv_sec;
+  gettimeofday(&now, NULL);
   // Create the BLE Device
-  BLEDevice::init("Fake iBeacon");
+  BLEDevice::init("No");
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
   pAdvertising = BLEDevice::getAdvertising();
@@ -73,8 +72,17 @@ void setup() {
   esp_deep_sleep(1000000LL * GPIO_DEEP_SLEEP_DURATION);
   Serial.printf("in deep sleep\n");
 }
+void setup() {
+  init();
+
+  if (1) {//start boardcasting or 
+    bluetoothboardcasting();
+  }else{}
+}
 
 
 void loop() {
+  //set up bus id, uint32_t busID, code here interrupt function
+  //
 
 }
