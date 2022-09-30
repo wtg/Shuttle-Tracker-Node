2aa8a60b4685e2f738586951617e87c25fb205ec
edited <1084929656lkh@gmail.com>
2022-09-29 22:01:58 -0400

    Update BLE_iBeacon.ino

--- BLE_iBeacon/BLE_iBeacon.ino
+++ BLE_iBeacon/BLE_iBeacon.ino
@@ -6,7 +6,6 @@
 #include "esp_sleep.h"
 
 
-
 #define GPIO_DEEP_SLEEP_DURATION     1  // sleep x seconds and then wake up
 //RTC SLOW MEME SECTION
 RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
@@ -15,7 +14,6 @@ RTC_DATA_ATTR static uint32_t busID = 0x0010;     // 16 bit major id
 
 
 BLEAdvertising *pAdvertising;   // BLE Advertisement type
-
 struct timeval now;
 
 // https://www.uuidgenerator.net/
@@ -26,20 +24,20 @@ void setBeacon() {
   BLEBeacon oBeacon = BLEBeacon();
   oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
   oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-  oBeacon.setMajor((bootcount & 0xFFFF0000) >> 16);//0x0102
+  oBeacon.setMajor(busID);
   Serial.printf("busID:%x\n", busID);
-  oBeacon.setMinor(bootcount & 0xFFFF);
+  oBeacon.setMinor(0x3);
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
   BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
   oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04, meaning not classic bluetooth
-  //std::string strServiceData = "";
-  //strServiceData += (char)26;     // Len
-  //strServiceData += (char)0xFF;   // Type
+  std::string strServiceData = "";
+  strServiceData += (char)26;     // Len
+  strServiceData += (char)0xFF;   // Type
   ////Customize message here
-  //strServiceData += oBeacon.getData(); 
+  strServiceData += oBeacon.getData(); 
   ////Customize message end
   ////concatinate customized message into servicedata
-  //oAdvertisementData.addData(strServiceData);
+  oAdvertisementData.addData(strServiceData);
   pAdvertising->setAdvertisementData(oAdvertisementData);
   pAdvertising->setScanResponseData(oScanResponseData);
 }
@@ -53,12 +51,12 @@ void init(){
 
 void bluetoothboardcasting(){
   digitalWrite(2, HIGH);
+  gettimeofday(&now, NULL);
   Serial.printf("start ESP32 %d\n", bootcount++);
   Serial.printf("deep sleep (%lds since last reset, %lds since last boot)\n", now.tv_sec, now.tv_sec - last);
   last = now.tv_sec;
-  gettimeofday(&now, NULL);
   // Create the BLE Device
-  BLEDevice::init("No");
+  BLEDevice::init("tracker");
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
   pAdvertising = BLEDevice::getAdvertising();
@@ -75,7 +73,6 @@ void bluetoothboardcasting(){
 }
 void setup() {
   init();
-
   if (1) {//start boardcasting or 
     bluetoothboardcasting();
   }else{}
