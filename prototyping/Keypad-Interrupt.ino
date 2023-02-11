36d85d4ae24543204855ba214b83edae6ba6aa8a
Wyatt Hauben <whauben11@gmail.com>
2023-02-10 22:24:29 -0500

    Converted button matrix code to interrupt paradigm.
    Needs to be tested once we have dev board for button matrix

---
+++ prototyping/Keypad-Interrupt.ino
@@ -0,0 +1,60 @@
+const int pin_row_1 = 3;
+const int pin_row_2 = 2;
+const int pin_row_3 = 1;
+const int pin_row_4 = 0;
+const int pin_col_1 = 6;
+const int pin_col_2 = 5;
+const int pin_col_3 = 4;
+
+const int bounce_interval = 100;
+const char button_vals[3][4] = {
+  {"1","2","3","#"},
+  {"4","5","6","*"},
+  {"7","8","9","0"},
+}
+
+volatile int bounceing[3][4] = {
+  {0,0,0,0},
+  {0,0,0,0},
+  {0,0,0,0},
+}
+
+void IRAM_ATTR updateKeypad(){
+  int rows[4] = {digitalRead(pin_row_1), digitalRead(pin_row_2), digitalRead(pin_row_3), digitalRead(pin_row_4)};
+  int cols[3] = {digitalRead(pin_col_1), digitalRead(pin_col_2), digitalRead(pin_col_3)};
+  int time = millis();
+
+  for(int i = 0; i < 4; ++i){
+    for(int j = 0; j < 3; ++j){
+      if(rows[i] == HIGH && cols[j] == LOW && time - bounceing[j][i] > bounce_interval){
+        Serial.println(button_vals[j][i]);
+        bounceing[j][i] = time;
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pin_row_1, INPUT_PULLDOWN);
+  pinMode(pin_row_2, INPUT_PULLDOWN);
+  pinMode(pin_row_3, INPUT_PULLDOWN);
+  pinMode(pin_row_4, INPUT_PULLDOWN);
+  pinMode(pin_col_1, INPUT_PULLUP);
+  pinMode(pin_col_2, INPUT_PULLUP);
+  pinMode(pin_col_3, INPUT_PULLUP);
+
+  attachInterrupt(pin_row_1, updateKeypad, CHANGE);
+  attachInterrupt(pin_row_2, updateKeypad, CHANGE);
+  attachInterrupt(pin_row_3, updateKeypad, CHANGE);
+  attachInterrupt(pin_row_4, updateKeypad, CHANGE);
+
+  attachInterrupt(pin_col_1, updateKeypad, CHANGE);
+  attachInterrupt(pin_row_2, updateKeypad, CHANGE);
+  attachInterrupt(pin_row_3, updateKeypad, CHANGE);
+
+}
+
+void loop() {
+
+}
