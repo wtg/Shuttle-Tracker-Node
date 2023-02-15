bf2cf6b00121a58c342a43bed1d7a16b332cbf9c
Wyatt Hauben <whauben11@gmail.com>
2023-02-14 22:43:07 -0500

    fixed keypad const array from string to char

--- prototyping/Keypad-Interrupt.ino
+++ prototyping/Keypad-Interrupt.ino
@@ -8,17 +8,18 @@ const int pin_col_3 = 4;
 
 const int bounce_interval = 100;
 const char button_vals[3][4] = {
-  {"1","2","3","#"},
-  {"4","5","6","*"},
-  {"7","8","9","0"},
-}
+  {'1','2','3','#'},
+  {'4','5','6','*'},
+  {'7','8','9','0'}
+};
 
 volatile int bounceing[3][4] = {
   {0,0,0,0},
   {0,0,0,0},
-  {0,0,0,0},
-}
-
+  {0,0,0,0}
+};
+volatile char last = ' ';
+volatile int changed = 0;
 void IRAM_ATTR updateKeypad(){
   int rows[4] = {digitalRead(pin_row_1), digitalRead(pin_row_2), digitalRead(pin_row_3), digitalRead(pin_row_4)};
   int cols[3] = {digitalRead(pin_col_1), digitalRead(pin_col_2), digitalRead(pin_col_3)};
@@ -27,7 +28,8 @@ void IRAM_ATTR updateKeypad(){
   for(int i = 0; i < 4; ++i){
     for(int j = 0; j < 3; ++j){
       if(rows[i] == HIGH && cols[j] == LOW && time - bounceing[j][i] > bounce_interval){
-        Serial.println(button_vals[j][i]);
+        last = Serial.println(button_vals[j][i]);
+        changed = 1;
         bounceing[j][i] = time;
       }
     }
@@ -48,13 +50,21 @@ void setup() {
   attachInterrupt(pin_row_2, updateKeypad, CHANGE);
   attachInterrupt(pin_row_3, updateKeypad, CHANGE);
   attachInterrupt(pin_row_4, updateKeypad, CHANGE);
-
+  
   attachInterrupt(pin_col_1, updateKeypad, CHANGE);
   attachInterrupt(pin_row_2, updateKeypad, CHANGE);
   attachInterrupt(pin_row_3, updateKeypad, CHANGE);
-
+  
 }
 
 void loop() {
-
+  if(changed == 1) {
+    Serial.print(last);
+    Serial.print('\0');
+    Serial.println();
+  }
+  else{
+    Serial.println("no");
+  }
+  delay(400);
 }
