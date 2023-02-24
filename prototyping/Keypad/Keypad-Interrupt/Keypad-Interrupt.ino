f6af78cd72687a14f2076858d82c7b21c434cc50
Wyatt Hauben <whauben11@gmail.com>
2023-02-24 17:32:04 -0500

    Working on interrupt keypad.
    needs work on pin assignment, Currently has chaotic behavior.

--- prototyping/Keypad/Keypad-Interrupt/Keypad-Interrupt.ino
+++ prototyping/Keypad/Keypad-Interrupt/Keypad-Interrupt.ino
@@ -1,10 +1,5 @@
-const int pin_row_1 = 3;
-const int pin_row_2 = 2;
-const int pin_row_3 = 1;
-const int pin_row_4 = 0;
-const int pin_col_1 = 6;
-const int pin_col_2 = 5;
-const int pin_col_3 = 4;
+const int cols[4] = {3,2,1,0};
+const int rows[3] = {6,5,4};
 
 const int bounce_interval = 100;
 const char button_vals[3][4] = {
@@ -18,43 +13,58 @@ volatile int bounceing[3][4] = {
   {0,0,0,0},
   {0,0,0,0}
 };
+
 volatile char last = ' ';
 volatile int changed = 0;
 void IRAM_ATTR updateKeypad(){
-  int rows[4] = {digitalRead(pin_row_1), digitalRead(pin_row_2), digitalRead(pin_row_3), digitalRead(pin_row_4)};
-  int cols[3] = {digitalRead(pin_col_1), digitalRead(pin_col_2), digitalRead(pin_col_3)};
+  Serial.println("MEOW");
   int time = millis();
-
-  for(int i = 0; i < 4; ++i){
-    for(int j = 0; j < 3; ++j){
-      if(rows[i] == HIGH && cols[j] == LOW && time - bounceing[j][i] > bounce_interval){
-        last = Serial.println(button_vals[j][i]);
-        changed = 1;
-        bounceing[j][i] = time;
+  if (time - bounceing[0][0] < bounce_interval) return;
+  for(int i = 0; i < 3; ++i){
+    if (digitalRead(cols[i]) == HIGH) continue;
+    pinMode(rows[i], OUTPUT);
+    digitalWrite(rows[i], LOW);
+    for(int j = 0; j < 4; ++j){
+      pinMode(cols[j], INPUT_PULLUP);
+      if(digitalRead(cols[j]) == LOW){
+        Serial.println(button_vals[i][j]);
       }
+      pinMode(cols[i], OUTPUT);
+      digitalWrite(cols[i], LOW);
+      /* if(rows[i] == HIGH && cols[j] == LOW && time - bounceing[j][i] > bounce_interval){ */
+      /*   last = Serial.println(button_vals[j][i]); */
+      /*   changed = 1; */
+      /*   bounceing[j][i] = time; */
+      /* } */
     }
+    pinMode(rows[i], INPUT_PULLUP);
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  pinMode(pin_row_1, INPUT_PULLDOWN);
-  pinMode(pin_row_2, INPUT_PULLDOWN);
-  pinMode(pin_row_3, INPUT_PULLDOWN);
-  pinMode(pin_row_4, INPUT_PULLDOWN);
-  pinMode(pin_col_1, INPUT_PULLUP);
-  pinMode(pin_col_2, INPUT_PULLUP);
-  pinMode(pin_col_3, INPUT_PULLUP);
 
-  attachInterrupt(pin_row_1, updateKeypad, CHANGE);
-  attachInterrupt(pin_row_2, updateKeypad, CHANGE);
-  attachInterrupt(pin_row_3, updateKeypad, CHANGE);
-  attachInterrupt(pin_row_4, updateKeypad, CHANGE);
+  pinMode(cols[0], OUTPUT);
+  pinMode(cols[1], OUTPUT);
+  pinMode(cols[2], OUTPUT);
+  pinMode(cols[3], OUTPUT);
+  digitalWrite(cols[0], LOW);
+  digitalWrite(cols[1], LOW);
+  digitalWrite(cols[2], LOW);
+  digitalWrite(cols[3], LOW);
 
-  // attachInterrupt(pin_col_1, updateKeypad, CHANGE);
-  // attachInterrupt(pin_row_2, updateKeypad, CHANGE);
-  // attachInterrupt(pin_row_3, updateKeypad, CHANGE);
+  pinMode(rows[0], INPUT_PULLUP);
+  pinMode(rows[1], INPUT_PULLUP);
+  pinMode(rows[2], INPUT_PULLUP);
+  /* attachInterrupt(pin_row_1, updateKeypad, CHANGE); */
+  /* attachInterrupt(pin_row_2, updateKeypad, CHANGE); */
+  /* attachInterrupt(pin_row_3, updateKeypad, CHANGE); */
+  /* attachInterrupt(pin_row_4, updateKeypad, CHANGE); */
 
+  attachInterrupt(rows[0], updateKeypad, CHANGE);
+  attachInterrupt(rows[1], updateKeypad, CHANGE);
+  attachInterrupt(rows[2], updateKeypad, CHANGE);
+  
 }
 
 void loop() {
